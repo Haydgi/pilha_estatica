10b767378ac7e84bba84c301a77ebe6e3be38f7f
Haydgi <90879611+Haydgi@users.noreply.github.com>
2022-09-05 17:21:17 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,117 +1,139 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define MAXTAM 1000
+#define INICIOARRANJO 0
+#define MAXTAM 1000 //definir o tamanho max
 
 typedef int TipoApontador;
+
 typedef struct
 {
-int num;
+    int mat;
+    char nome[90];
+    char cargo[50];
+    float salario;
 
-} TipoItem;
+} Funcionario;
 typedef struct
 {
 
-TipoItem Item[MAXTAM];
-TipoApontador Topo;
-} TipoPilha;
+Funcionario Item[MAXTAM];
+TipoApontador Primeiro, Ultimo;
+} TipoLista;
 
+void FLVazia(TipoLista *Lista)
 
-void FPVazia(TipoPilha *Pilha){ Pilha -> Topo = 0; }
+{ Lista->Primeiro = INICIOARRANJO;  Lista->Ultimo = Lista->Primeiro ; }
 
-int Vazia(TipoPilha Pilha){ 
-   return ( Pilha .Topo == 0); }
+////////////////////////
 
-void Empilha(TipoItem x, TipoPilha *Pilha){ 
-    if ( Pilha -> Topo == MAXTAM) printf ( "Erro : pilha esta cheia\n" );
-    else { Pilha -> Topo++; 
-    Pilha -> Item[Pilha -> Topo - 1] = x ; }
-
-}
-void Desempilha(TipoPilha *Pilha , TipoItem *Item){ 
-if (Vazia(*Pilha )) printf ( "Erro : pilha esta vazia\n" );
-else{ 
-*Item = Pilha->Item[Pilha->Topo- 1]; 
-Pilha->Topo --; 
-}
+int Vazia(TipoLista Lista)
+{ return ( Lista.Primeiro == Lista.Ultimo ) ; 
 }
 
-void Imprime_baixoCima (TipoPilha *Pilha){
-    int i;
-    if (Vazia(*Pilha)) {
-        printf("A pilha está vazia!\n");
+void Insere(Funcionario x, TipoLista *Lista){
+    
+if ( Lista -> Ultimo > MAXTAM){
+printf ( "Lista esta cheia\n" );
+}else {
+Lista -> Item[ Lista->Ultimo] = x;
+Lista -> Ultimo++;
+}
+
+}
+
+void Imprime(TipoLista Lista){
+    if(Vazia(Lista)){
+        printf(">>>A fila está vazia!<<<\n\n\n");
     }else{
-    for (i = 0; i < Pilha->Topo; i++) {
-        printf("%d\n",Pilha->Item[i].num);
-
-    }
-    }
+ int Aux;
+ for (Aux = Lista.Primeiro; Aux <= (Lista.Ultimo - 1); Aux++){
+printf ( "MATRICULA: %d\n" , Lista.Item[Aux].mat);
+printf ( "NOME: %s\n" , Lista.Item[Aux].nome);
+printf ( "CARGO: %s\n" , Lista.Item[Aux].cargo);
+printf ( "SALARIO:R$%.2f\n\n\n" , Lista.Item[Aux].salario);
+ }
+}
 }
 
-void Imprime_cimaBaixo (TipoPilha *Pilha){
-    int i;
-    if (Vazia(*Pilha)) {
-        printf("A pilha está vazia!\n");
-    }else{
-
-    for (i = Pilha->Topo-1; i >= 0; i--) {
-        printf("%d\n",Pilha->Item[i].num);
-
-    }
-    }
+void Retira(TipoApontador p, TipoLista *Lista , Funcionario *Item){ 
+int Aux;
+if (Vazia(*Lista ) || p >= Lista -> Ultimo){ 
+    printf ( "Erro : Posicao nao existe \n" );
+    return;
 }
 
+*Item = Lista -> Item[p];
+
+Lista -> Ultimo--;
+for (Aux = p; Aux < Lista -> Ultimo ; Aux++){
+Lista -> Item[Aux] = Lista -> Item[Aux] ;
+}
+
+}
 
 int main(int argc, char** argv) {
     int menu = 0;
-    TipoPilha pilhaN;
-    TipoItem Numero;
+    int reg = 1;
+    //int i = 0;
+    Funcionario ref;
+    TipoLista listaN;
+    
+    
+    
     
     while (menu != 56) {
-    printf("======MENU=========\n");
-    
-    printf("1 - Cria Pilha\n");
-    printf("2 - Empilha\n");
-    printf("3 - Desempilha\n");
-    printf("4 - Imprime de baixo pra cima Pilha\n");
-    printf("5 - Imprime de cima pra baixo Pilha\n");
+
+        printf("/******************/\n"
+           "********MENU*******\n"
+           "/******************/\n");
+    printf("1 - Cria Lista\n");
+    printf("2 - Insere Lista\n");
+    printf("3 - Imprime Lista\n");
+    printf("4 - Remove elementoo Lista\n");
     printf("56 - Sai do progama\n");
+        
+    printf("Insira a opção desejada:");
     scanf("%d",&menu);
     
-    switch(menu){
-        case 1:
-            FPVazia(&pilhaN);
-            printf("Pilha criada =D\n\n");
-            break;
-            
-        case 2:
-            printf("Qual numero deseja empilhar?\n");
-            scanf("%d", &Numero.num);
-            Empilha(Numero,&pilhaN);
-            printf("Numero Empilhado! =D\n\n");
-            break;
-            
-        case 3:
-            Desempilha(&pilhaN, &Numero);
-            printf("Item removido da pilha =D\n\n");
-            
-            break;
-            
+    switch (menu)
+{
+   case 1:
+       FLVazia(&listaN);
+       printf("\n\nLISTA CRIADA!\n\n");
+   break;
+
+   case 2:
+       ref.mat = reg;
+       printf("Insira o nome do funcionario\n");
+       scanf("%s",&ref.nome);
+       printf("Insira o cargo do funcionario\n");
+       scanf("%s",&ref.cargo);
+       printf("Insira o salario do funcionario\n");
+       scanf("%f",&ref.salario);    
+       Insere(ref,&listaN);
+       reg++; 
+   break;
+   
+    case 3:
+        Imprime(listaN);
+        break;    
+        
         case 4:
-            printf("===PILHA===\n\n");
-            Imprime_baixoCima(&pilhaN);
+            int diresq = 0;
+                Retira (listaN.Primeiro,&listaN,&ref); //não está funcionando ainda
+                printf("Elemento Removido!\n\n");      
+        break;
+   
+        case 56:
+            printf("Tchau\n");
             break;
-            
-        case 5:
-            printf("===PILHA===\n\n");
-            Imprime_cimaBaixo(&pilhaN);
-            break;
-                
-            
+
+        default:
+           printf("opção invalida");
+}
+    //i++;
     
-    
-    }
     }
     return (EXIT_SUCCESS);
 }
-
